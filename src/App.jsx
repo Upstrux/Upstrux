d2f129f120995304f9ef899249f29a25abd3f8f9
Upstrux <teimour.1988@yahoo.com>
2026-05-11 22:51:22 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 
-import structuralAnalysisDesign from "./assets/structural-Analysis-Проектиране.png";
+import structuralAnalysisDesign from "./assets/Structural-Analysis-Design.png";
 import buildingTechnology from "./assets/building-technology.jpg";
 import constructionManagement from "./assets/construction-management.jpg";
 
