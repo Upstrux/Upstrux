05dd2493b04111bf4ee8dbab3f1c12b2bdb9ea65
Upstrux <teimour.1988@yahoo.com>
2026-05-22 22:33:25 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -296,14 +296,6 @@ function runContentChecks() {
 
 runContentChecks();
 
-function SectionDivider({ className = "" }) {
-  return (
-    <div
-      className={`mx-auto h-px w-full max-w-7xl bg-gradient-to-r from-transparent via-slate-300 to-transparent ${className}`}
-    />
-  );
-}
-
 function HeroSlide({ image, index }) {
   return (
     <motion.div
