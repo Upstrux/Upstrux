dcb270c5e1443417359a25162beed627c0098ca3
Upstrux <teimour.1988@yahoo.com>
2026-05-24 19:55:51 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
 //About
-import hero1 from "./assets/hero/hero1.webp";
+
 //hero
 import hero1 from "./assets/hero/hero1.webp";
 import hero2 from "./assets/hero/hero2.webp";
