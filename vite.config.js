d86f24742d30bfc5712e968fe2f1a2cce5d8878d
Upstrux <teimour.1988@yahoo.com>
2026-06-26 12:00:32 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false,      // Don't generate source maps
+    minify: "esbuild",     // Minify JavaScript
+    cssMinify: true,       // Minify CSS
+  },
 });
